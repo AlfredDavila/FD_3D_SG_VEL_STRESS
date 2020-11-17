50f157af3135d295f7984f7abbc0ed334a4a73d5
unknown <daga9110@gmail.com>
2020-11-17 14:32:41 -0600

    First commit, 4 order not works

--- src/operators/diff_operator.py
+++ src/operators/diff_operator.py
@@ -1,11 +1,120 @@
 import numpy as np
+import matplotlib.pyplot as plt
+#https://numpydoc.readthedocs.io/en/latest/format.html
 
 
+def diff_2(field, N, dx, order):
+    """Made the derivate of a plane in one direction
 
-def diff(field_value, N):
-	"""
-		Made the derivate of a plane in one direction
-		field_value Is 
-		N 			Number of element per row
-	"""
-	pass
+    1. First derivate all the field 
+    2. Then corrects the boundary values
+    3. Return the derivate field
+
+    Function used in the finite differences formulation
+    for the seismic elastic wave equation
+
+    Parameters
+    ----------
+    field : ndarray
+        The field (Stress, Velocity ....)
+    N : int
+        Number of elements per row
+    dx : number
+        Delta x in the derivation
+    order : int 
+        Order of the differential operator
+
+    Returns
+    -------
+    ndarray
+        A ndarray with the second derivate of the field
+    """
+	
+    #Validate order
+    if order not in [2,4]:
+
+        raise ValueError('Order not valid: {0}'.format(order)) 
+        
+
+    #Firts derivate all the field
+    indexes = np.arange(0, len(field))
+
+    prev_idxs = indexes - 1
+
+    post_idxs = indexes + 1
+
+    post_idxs[-1] = 0
+
+    #For order 4
+    prev_prev_idxs = indexes - 1
+
+    post_post_idxs = indexes + 1
+
+    post_post_idxs[-2:] = 0
+
+    c0 = 1.125
+
+    c1 = 1/24
+
+    dx_inv = 1/(dx*2)
+
+    if order == 2:
+
+        field_diff = field[post_idxs] - field[prev_idxs]
+
+    elif order == 4:
+        # @todo no da buenos resultados, revisar
+
+        field_diff = c0 * (field[post_idxs] - field[prev_idxs]) 
+        -  c1 *(field[post_post_idxs] - field[prev_prev_idxs])
+
+    ##Then corrects the boundary values
+    boundary_idx = np.arange(0, len(field), N)
+
+    # In order 2 the boundary index are iN and iN-1 (i integer)
+
+    boundary_idx_prev = boundary_idx - 1
+
+    field_diff[boundary_idx] = 0.0
+    field_diff[boundary_idx_prev] = 0.0
+
+    # In order 4 the boundary index are the same of order 2 plus iN+1 and iN-2 (i integer)
+    if order == 4:
+        boundary_idx_prev_prev = boundary_idx - 2
+        boundary_idx_post = boundary_idx + 1
+        boundary_idx_post[-1] = 0
+
+        field_diff[boundary_idx_prev_prev] = 0.0
+        field_diff[boundary_idx_post] = 0.0
+
+    #Return the derivate field
+    field_diff = dx_inv * field_diff
+
+    return field_diff
+
+dx = 0.01
+
+x = np.arange(0,2 * np.pi,dx)
+
+y = np.sin(x)
+
+y_diff = np.cos(x)
+
+field_diff_a = y_diff
+
+field = y
+
+N_rows = 3
+
+for i in range(N_rows-1):
+    field = np.concatenate((field,y))
+
+    field_diff_a = np.concatenate((field_diff_a,y_diff))
+
+field_diff = diff_2(field, len(x), dx, 2)
+
+x_plot = np.arange(0, dx * len(x) * N_rows, dx)
+
+plt.plot(x_plot, field_diff)
+plt.plot(x_plot, field_diff_a)
+plt.show()
